6c3e81bbbb3e061b71404d4a9159b8192ac7668a
Ritik Jain <ritikjain247@gmail.com>
2023-06-01 17:27:44 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+import gql from 'graphql-tag';
+import { ApolloServer } from 'apollo-server';
+
+const typeDefs = gql`
+  type User {
+    name: String
+    email: String
+  }
+
+  type Query {
+    getUser: User
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getUser: (parent, args, context) => {
+      const authHeaders = context.headers.authorization || "";
+      const token = authHeaders.replace("Bearer ", "");
+      try {
+        if (!token) {
+          return "Authorization token is missing!";
+        }
+        console.log("in custom resolver");
+        return "hello from custom resolver";
+      } catch (e) {
+        console.log("in custom resolver", e);
+        return null;
+      }
+    }
+  }
+};
+
+const context = ({ req }) => {
+  return { headers: req.headers };
+};
+
+const schema = new ApolloServer({ typeDefs, resolvers, context });
+schema.listen({ port: 8080 }).then(({ url }) => {
+  console.log(`schema ready at ${url}`);
+});
